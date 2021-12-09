1ec1a0c526404a7edef05636b9f2554acf25513b
ejimenez123 <caribestic123@gmail.com>
2021-12-09 10:21:29 -0500

    fix: Config router history at Nginx

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -22,5 +22,10 @@ class DatabaseSeeder extends Seeder
           ]
         );
         \App\Models\User::factory(20)->create();
+        
+        $this->call([
+            //UserSeeder::class,
+            ProductSeeder::class,
+        ]);
     }
 }
